0c7b3912545a182ce96be4d5807625e6736b5396
Michelle Wiraydi <miwiryadi@gmail.com>
2018-01-04 19:13:46 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+ 
+    num_1 = Kernel.rand(0..24)
+    
+ 
+    resp.write "#{num_1}\n"
+  
+ 
+    if num_1< 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+ 
+    resp.finish
+  end
+ 
+end
